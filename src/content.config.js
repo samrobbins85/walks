68be6b2bce49ab7fcdbe2775c893fac31baa1c1c
Sam Robbins <samrobbinsgb@gmail.com>
2025-05-24 21:31:46 +0100

    Homepage cards

--- src/content.config.js
+++ src/content.config.js
@@ -1,17 +1,20 @@
 import { defineCollection, reference, z } from "astro:content";
 
 // 2. Import loader(s)
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
 // 3. Define your collection(s)
 const walks = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/walks" }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    description: z.string(),
-    geojson: reference("walkGeoJSON"),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      description: z.string(),
+      geojson: reference("walkGeoJSON"),
+      cover: image(),
+      date: z.date(),
+    }),
 });
 
 const walkGeoJSON = defineCollection({
